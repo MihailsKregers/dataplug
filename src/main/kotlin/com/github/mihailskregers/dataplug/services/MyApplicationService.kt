0ecf7c44e5779c4e4cc56ae39a85004dfe2048be
GitHub Action <action@github.com>
2021-12-01 16:36:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/mihailskregers/dataplug/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/mihailskregers/dataplug/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.mihailskregers.dataplug.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mihailskregers.dataplug.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/mihailskregers/dataplug/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mihailskregers/dataplug/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.mihailskregers.dataplug.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mihailskregers.dataplug.MyBundle
 
 class MyProjectService(project: Project) {
 
